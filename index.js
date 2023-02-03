de5f916c87a76ea9b3e273a8de73bd7237aa42b8
Slaya45 <caydenfrye@gmail.com>
2023-02-02 21:32:31 -0700

    Practicing java while following an online learning video

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-// This is my first Java Script code!
-console.log('Hello World!')
 
-alert ("Hello World!");
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+const audio = new Audio("bock.mp3");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
+let count = 0
+ console.log(saveEl)
+
+function increment() {
+ 
+    count += 1
+    countEl.textContent = count
+}
+
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    console.log(count)
+    countEl.textContent = 0
+    count = 0
+}
+
+function xTen() {
+  count +=  10
+  countEl.textContent = count 
+
+
+
+
+
+}
+
+
+let saveBtn = document.getElementById("save")
+
+
+
+
+
+  
